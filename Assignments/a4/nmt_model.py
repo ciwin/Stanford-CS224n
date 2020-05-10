4333502eab00e92629e02a74da9561a0ecc1d47e
Christoph Windheuser <cwindheu@thoughtworks.com>
2020-05-10 19:28:13 +0200

    Finished exercise 1e

--- Assignments/a4/nmt_model.py
+++ Assignments/a4/nmt_model.py
@@ -260,10 +260,16 @@ class NMT(nn.Module):
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
 
-
-
-
-
+        enc_hiddens_proj = self.att_projection(enc_hiddens)
+        Y = self.model_embeddings.target(target_padded)
+        for Y_t in torch.split(Y, split_size_or_sections=1, dim=0):
+            Y_t = torch.squeeze(Y_t, dim=0)
+            Ybar_t = torch.cat((Y_t, o_prev), dim=1)
+            #dec_state = self.decoder(Ybar_t, dec_state)
+            dec_state, o_t, e_t =self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs, dim=0)
 
         ### END YOUR CODE
 
